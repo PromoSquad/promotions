8bebddfad2419dd0a5363821c549cecde4a41233
Dizy <dev@dizy.cc>
2021-10-22 04:51:46 -0400

    17 | Ziyang | Fill test file with unittest testcase structure

---
+++ tests/test_routes.py
@@ -0,0 +1,25 @@
+import unittest
+
+class TestPromotionServer(unittest.TestCase):
+    """ Promotion Server Tests """
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    ######################################################################
+    #  T E S T   C A S E S
+    ######################################################################
+
+    def test_index(self):
+        pass
