28ef3478e5427b86f7719c5f9a3a7ab8e166cd44
Dizy <dev@dizy.cc>
2021-10-22 18:51:43 -0400

    #38 | Ziyang | Add error_handlers.py for error handling

--- service/routes.py
+++ service/routes.py
@@ -1,9 +1,17 @@
 from . import app
 from .models import Promotion
+from flask import Flask, jsonify, request, url_for, make_response, abort
+from . import status
 
 @app.route('/')
 def index():
-    return "<p>Hello, World2!</p>"
+    return (
+        jsonify(
+            name="Promotion REST API Service",
+            version="1.0"
+        ),
+        status.HTTP_200_OK
+    )
 
 def init_db():
     """ Initialize the SQLAlchemy app """
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,25 +1,47 @@
+import os
+import logging
 import unittest
+from service import status
+from service.models import db
+from service.routes import app, init_db
+
+DATABASE_URI = os.getenv(
+    "DATABASE_URI", "postgres://postgres:postgres@localhost:5432/postgres"
+)
+
+BASE_URL = '/promotions'
+
 
 class TestPromotionServer(unittest.TestCase):
     """ Promotion Server Tests """
 
     @classmethod
     def setUpClass(cls):
-        pass
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        app.logger.setLevel(logging.CRITICAL)
+        init_db()
 
     @classmethod
     def tearDownClass(cls):
         pass
 
     def setUp(self):
-        pass
+        db.drop_all()
+        db.create_all()
+        self.app = app.test_client()
 
     def tearDown(self):
-        pass
+        db.session.remove()
+        db.drop_all()
 
     ######################################################################
     #  T E S T   C A S E S
     ######################################################################
 
     def test_index(self):
-        pass
+        resp = self.app.get("/")
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], "Promotion REST API Service")
