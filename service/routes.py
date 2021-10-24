4999b89a19bf41e2c3343ad1355c953cde6c023d
tz2182 <79621052+tz2182@users.noreply.github.com>
2021-10-24 15:00:48 -0400

    #12 Tingting updatePromotion API

--- service/routes.py
+++ service/routes.py
@@ -51,6 +51,25 @@ def delete_promotions(id):
         promotion.delete()
     return make_response("", status.HTTP_204_NO_CONTENT)
 
+######################################################################
+# UPDATE AN EXISTING PROMOTION
+######################################################################
+@app.route("/promotions/<int:id>", methods=["PUT"])
+def update_promotions(id):
+    """
+    Update a Promotion
+    This endpoint will update a Promotion based the body that is posted
+    """
+    app.logger.info("Request to update promotion with id: %s", id)
+    check_content_type("application/json")
+    promotion = Promotion.find(id)
+    if not promotion:
+        raise NotFound("Promotion with id '{}' was not found.".format(id))
+    promotion.deserialize(request.get_json())
+    promotion.id = id
+    promotion.update()
+    return make_response(jsonify(promotion.serialize()), status.HTTP_200_OK)
+
 
 def init_db():
     """ Initialize the SQLAlchemy app """
